c6366c0ee7c54887dbc06875b607a574dc884753
Henrique Moco <hmoco94@vt.edu>
2017-01-05 16:18:34 -0500

    renamed some components and mixins

---
+++ app/components/search-registries.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    metrics: Ember.inject.service(),
+    actions: {
+        search() {
+            let query = Ember.$.trim(this.$('#searchBox').val());
+            this.sendAction('search', query);
+            Ember
+                .get(this, 'metrics')
+                .trackEvent({
+                    category: 'button',
+                    action: 'click',
+                    label: 'Registries - Index - Search'
+                });
+        }
+    },
+
+    keyDown(event) {
+        // Search also activated when enter key is clicked
+        if (event.keyCode === 13) {
+            this.send('search');
+        }
+    }
+});
