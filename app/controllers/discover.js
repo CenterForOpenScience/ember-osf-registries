0b261f52f64b412680a42a1d408b78ead0aa33ce
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-03-13 15:30:42 -0400

    Use discover-page component on discover page.

--- app/controllers/discover.js
+++ app/controllers/discover.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
-import config from 'ember-get-config';
 import Analytics from '../mixins/analytics';
 import RegistrationCount from '../mixins/registration-count';
-
-import { elasticEscape } from '../utils/elastic-query';
-
-const filterMap = {
-    providers: 'sources',
-    types: 'registration_type'
-};
 
 export default Ember.Controller.extend(Analytics, RegistrationCount, {
     i18n: Ember.inject.service(),
@@ -22,10 +14,12 @@ export default Ember.Controller.extend(Analytics, RegistrationCount, {
         return this.get('i18n').t('discover.main.active_filters.button');
     }),
     consumingService: 'registries', // Consuming service - registries here
-    detailRoute: 'content',
+    discoverHeader: Ember.computed('i18n', function() { // Header for registries discover page
+        return this.get('i18n').t('index.header.title.paragraph');
+    }),
     facets: [ // List of facets available for registries
         { key: 'sources', title: 'Providers', component: 'search-facet-provider' },
-        { key: 'registration_type', title: 'OSF Registration Type', component: '' }
+        { key: 'registration_type', title: 'OSF Registration Type', component: 'search-facet-registration-type' }
     ],
     filterMap: { // Map active filters to facet names expected by SHARE
         providers: 'sources',
@@ -41,13 +35,7 @@ export default Ember.Controller.extend(Analytics, RegistrationCount, {
     page: 1,
     provider: '',
     q: '',
-    // queryParams: ['page', 'q', 'provider', 'type'],
-    queryParams: {
-        page: 'page',
-        queryString: 'q',
-        typeFilter: 'type',
-        providerFilter: 'provider',
-    },
+    queryParams: ['page', 'q', 'provider', 'type'],
     searchButton: Ember.computed('i18n', function() { // Search button text
         return this.get('i18n').t('global.search');
     }),
@@ -67,47 +55,8 @@ export default Ember.Controller.extend(Analytics, RegistrationCount, {
     }],
     type: '',
 
-
-
-    osfProviders: [
-        'OSF',
-        // 'AEA Registry', //These need to be added to the language filter once on SHARE (OSF -> OSF Registries)
-        // 'ANZCTR',
-        // 'Clinicaltrials.gov',
-        // 'EGAP',
-        // 'EU Clinical Trials',
-        // 'ISRCTN',
-        // 'Research Registry',
-        // 'RIDIE'
-    ],
-
-    size: 10,
-    numberOfResults: 0,
-    queryString: '',
-    queryBody: {},
-    providersPassed: false,
-
-    sortByOptions: ['Relevance', 'Upload date (oldest to newest)', 'Upload date (newest to oldest)'],
-
-    // chosenOption is always the first element in the list
-    chosenSortByOption: Ember.computed('sortByOptions', function() {
-        return this.get('sortByOptions')[0];
-    }),
-
-    showPrev: Ember.computed.gt('page', 1),
-    showNext: Ember.computed('page', 'size', 'numberOfResults', function() {
-        return this.get('page') * this.get('size') < this.get('numberOfResults');
-    }),
-
-    results: Ember.ArrayProxy.create({ content: [] }),
-
-    searchUrl: config.SHARE.searchUrl,
-
     init() {
         this._super(...arguments);
-        this.set('facetFilters', Ember.Object.create());
-
-        this.loadPage();
         Ember.run.schedule('afterRender', () => {
             let providers = this.get('activeFilters.providers');
             if (providers.length === 1 && providers[0] === 'OSF') {
@@ -145,231 +94,5 @@ export default Ember.Controller.extend(Analytics, RegistrationCount, {
             Ember.$('.type-checkbox').attr('disabled', 'disabled');
             Ember.$('.registration-type-selector').fadeTo('slow', 0.5);
         }
-    },
-    loadPage() {
-        this.set('loading', true);
-        Ember.run.debounce(this, this._loadPage, 500);
-    },
-    _loadPage() {
-        let queryBody = JSON.stringify(this.getQueryBody());
-
-        return Ember.$.ajax({
-            url: this.get('searchUrl'),
-            crossDomain: true,
-            type: 'POST',
-            contentType: 'application/json',
-            data: queryBody
-        }).then(json => {
-            if (this.isDestroyed || this.isDestroying) return;
-
-            this.set('numberOfResults', json.hits.total);
-
-            let results = json.hits.hits.map(hit => {
-                // HACK: Make share data look like apiv2 preprints data
-                let result = Ember.merge(hit._source, {
-                    id: hit._id,
-                    type: 'elastic-search-result',
-                    workType: hit._source['@type'],
-                    providers: hit._source.sources.map(item => ({name: item})),
-                    osfProvider: hit._source.sources.reduce((acc, source) => (acc || this.get('osfProviders').includes(source)), false),
-                    hyperLinks: [// Links that are hyperlinks from hit._source.lists.links
-                        {
-                            type: 'share',
-                            url: config.SHARE.baseUrl + 'preprint/' + hit._id
-                        }
-                    ],
-                    infoLinks: [], // Links that are not hyperlinks  hit._source.lists.links
-                    registrationType: hit._source.registration_type
-                });
-
-                hit._source.identifiers.forEach(function(identifier) {
-                    if (identifier.startsWith('http://')) {
-                        result.hyperLinks.push({url: identifier});
-                    } else {
-                        const spl = identifier.split('://');
-                        const [type, uri, ..._] = spl; // jshint ignore:line
-                        result.infoLinks.push({type, uri});
-                    }
-                });
-
-                result.contributors = result.lists.contributors ? result.lists.contributors
-                  .sort((b, a) => (b.order_cited || -1) - (a.order_cited || -1))
-                  .map(contributor => ({
-                        users: Object.keys(contributor)
-                          .reduce(
-                              (acc, key) => Ember.merge(acc, {[key.camelize()]: contributor[key]}),
-                              {bibliographic: contributor.relation !== 'contributor'}
-                          )
-                    })) : [];
-
-                // Temporary fix to handle half way migrated SHARE ES
-                // Only false will result in a false here.
-                result.contributors.map(contributor => contributor.users.bibliographic = !(contributor.users.bibliographic === false));  // jshint ignore:line
-
-                return result;
-            });
-
-            this.set('loading', false);
-            return this.set('results', results);
-        });
-    },
-    maxPages: Ember.computed('numberOfResults', function() {
-        return ((this.get('numberOfResults') / this.get('size')) | 0) + (this.get('numberOfResults') % 10 === 0 ? 0 : 1);
-    }),
-    getQueryBody() {
-        const facetFilters = this.get('activeFilters');
-
-        this.set('typeFilter', facetFilters.types.join('AND'));
-
-        if (!this.get('theme.isProvider'))
-            this.set('providerFilter', facetFilters.providers.join('AND'));
-
-        const filter = [
-            {
-                terms: {
-                    type: [
-                        'registration'
-                    ]
-                }
-            }
-        ];
-
-        // TODO set up ember to transpile Object.entries
-        for (const key in filterMap) {
-            const val = filterMap[key];
-            const filterList = facetFilters[key];
-
-            if (!filterList.length || (key === 'providers' && this.get('theme.isProvider')))
-                continue;
-
-            filter.push({
-                terms: {
-                    [val]: filterList
-                }
-            });
-        }
-
-        if (this.get('theme.isProvider')) {
-            filter.push({
-                terms: {
-                    sources: [this.get('theme.provider.name')]
-                }
-            });
-        }
-
-        const sortByOption = this.get('chosenSortByOption');
-        const sort = {};
-
-        if (sortByOption === 'Upload date (oldest to newest)') {
-            sort.date_published = 'asc';
-        } else if (sortByOption === 'Upload date (newest to oldest)') {
-            sort.date_published = 'desc';
-        }
-
-        return this.set('queryBody', {
-            query: {
-                bool: {
-                    must: {
-                        query_string: {
-                            query: elasticEscape(this.get('queryString')) || '*'
-                        }
-                    },
-                    filter
-                }
-            },
-            sort,
-            from: (this.get('page') - 1) * this.get('size'),
-        });
-    },
-
-    reloadSearch: Ember.observer('activeFilters', function() {
-        this.set('page', 1);
-        this.loadPage();
-    }),
-    otherProviders: [],
-    actions: {
-        search(val, event) {
-            if (event &&
-                (
-                    event.keyCode < 49 ||
-                    [91, 92, 93].includes(event.keyCode)
-                ) &&
-                ![8, 32, 48].includes(event.keyCode)
-            )
-                return;
-
-            this.set('page', 1);
-            this.loadPage();
-
-            Ember.get(this, 'metrics')
-                .trackEvent({
-                    category: `${event && event.type === 'keyup' ? 'input' : 'button'}`,
-                    action: `${event && event.type === 'keyup' ? 'onkeyup' : 'click'}`,
-                    label: 'Registries -  Discover - Search'
-                });
-        },
-
-        previous() {
-            if (this.get('page') > 1) {
-                this.decrementProperty('page');
-                this.loadPage();
-            }
-        },
-
-        next() {
-            if (this.get('page') * this.get('size') <= this.get('numberOfResults')) {
-                this.incrementProperty('page');
-                this.loadPage();
-            }
-        },
-
-        clearFilters() {
-            this.set('activeFilters', {
-                providers: this.get('theme.isProvider') ? this.get('activeFilters.providers') : [],
-                types: []
-            });
-
-            Ember.get(this, 'metrics')
-                .trackEvent({
-                    category: 'button',
-                    action: 'click',
-                    label: 'Registries -  Discover - Clear Filters'
-                });
-        },
-
-        sortBySelect(index) {
-            // Selecting an option just swaps it with whichever option is first
-            let copy = this.get('sortByOptions').slice(0);
-            let temp = copy[0];
-            copy[0] = copy[index];
-            copy[index] = temp;
-            this.set('sortByOptions', copy);
-            this.set('page', 1);
-            this.loadPage();
-
-            Ember.get(this, 'metrics')
-                .trackEvent({
-                    category: 'dropdown',
-                    action: 'select',
-                    label: `Registries -  Discover - Sort by: ${copy[index]}`
-                });
-        },
-
-        updateFilters(filterType, item) {
-            item = typeof item === 'object' ? item.text : item;
-            const filters = Ember.$.extend(true, [], this.get(`activeFilters.${filterType}`));
-            const hasItem = filters.includes(item);
-            const action = hasItem ? 'remove' : 'push';
-            filters[`${action}Object`](item);
-            this.set(`activeFilters.${filterType}`, filters);
-            this.notifyPropertyChange('activeFilters');
-
-            Ember.get(this, 'metrics')
-                .trackEvent({
-                    category: 'filter',
-                    action: hasItem ? 'remove' : 'add',
-                    label: `Registries -  Discover - ${filterType} ${item}`
-                });
-        },
-    },
+    }
 });
