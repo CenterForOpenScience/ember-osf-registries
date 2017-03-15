3cd6ba308e0b41eb55d9008ab3458c05cca206f3
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-03-15 14:23:02 -0400

    Clear activeFilters when transitioning from discover page.

--- app/controllers/discover.js
+++ app/controllers/discover.js
@@ -106,5 +106,12 @@ export default Ember.Controller.extend(Analytics, RegistrationCount, {
             Ember.$('.type-checkbox').attr('disabled', 'disabled');
             Ember.$('.registration-type-selector').fadeTo('slow', 0.5);
         }
+    },
+     // TODO _clearFilters has been moved to the Ember-OSF discover-page component.  Better way than duplicating this here?
+    _clearFilters() {
+        this.set('activeFilters', {
+            providers: this.get('theme.isProvider') ? this.get('activeFilters.providers') : [],
+            types: []
+        });
     }
 });
