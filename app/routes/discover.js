a4adfcff5c0ba722a731b7c2e99fae2af355532d
Lauren Barker <lauren@cos.io>
2018-06-28 11:23:49 -0400

    [IN-305][Registries] Fix LiveData (by refactoring the discover page) (#64)
    * Update registries to work with discover page refactor
    
    * Fix search facet tests
    
    * Use most recent ember-osf version

--- app/routes/discover.js
+++ app/routes/discover.js
@@ -3,22 +3,10 @@ import Analytics from 'ember-osf/mixins/analytics';
 import ResetScrollMixin from '../mixins/reset-scroll';
 
 export default Route.extend(Analytics, ResetScrollMixin, {
-    queryParams: {
-        queryString: {
-            replace: true,
-        },
-    },
     model() {
         return this
             .get('store')
             .findAll('preprint-provider', { reload: true })
             .then(result => result.filter(item => item.id !== 'osf'));
-    },
-    actions: {
-        willTransition() {
-            const controller = this.controllerFor('discover');
-            controller._clearFilters();
-            controller._clearQueryString();
-        },
     },
 });
